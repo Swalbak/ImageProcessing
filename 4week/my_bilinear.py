9a7b95b591607574e6e4371340f47509025b76f0
swalbak <usually670@gmail.com>
2023-04-05 08:55:07 +0900

    4w

--- 4week/my_bilinear.py
+++ 4week/my_bilinear.py
@@ -53,8 +53,40 @@ def my_bilinear(src, scale):
 
     return dst
 
+def my_nearest(src, scale):
+
+    (h, w) = src.shape
+    
+    # 0.5를 더한 값에서 버림 진행 -> 반올림과 같음
+    # 이미지 확장 시 원본 픽셀을 넘어서는 픽셀값이 생길 수 있음
+    h_dst = int(h * scale + 0.5)
+    w_dst = int(w * scale + 0.5)
+
+
+    dst = np.zeros((h_dst, w_dst))
+
+    for row in range(h_dst):
+        for col in range(w_dst):
+            y = row / scale
+            x = col / scale
+
+            # m, n: 기존 이미지의 픽셀 좌표(반올림)
+            m = int(y + 0.5)
+            n = int(x + 0.5)
+            
+            if m >= h:
+                m = h - 1
+            
+            if n >= w:
+                n = w - 1
+            
+            dst[row, col] = src[m, n]
+
+    return dst
+
 if __name__ == '__main__':
     src = cv2.imread('Lena.png', cv2.IMREAD_GRAYSCALE)
+    cv2.imshow('[20191780 Yuk Jung Hun]original', src)
 
     scale = 3
     #이미지 크기 ??x??로 변경
@@ -66,9 +98,9 @@ if __name__ == '__main__':
     my_dst = my_dst.astype(np.uint8)
 
     # 출력 윈도우에 학번과 이름을 써주시기 바립니다.
-    cv2.imshow('[20191780 육정훈]original', src)
-    cv2.imshow('[20191780 육정훈]my bilinear mini', my_dst_mini)
-    cv2.imshow('[20191780 육정훈]my bilinear', my_dst)
+    cv2.imshow('[20191780 Yuk Jung Hun]original', src)
+    cv2.imshow('[20191780 Yuk Jung Hun]my bilinear mini', my_dst_mini)
+    cv2.imshow('[20191780 Yuk Jung Hun]my bilinear', my_dst)
 
     cv2.waitKey()
     cv2.destroyAllWindows()
